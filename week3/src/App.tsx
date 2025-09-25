43c85d8526f31a0565eed7b69aa8d4e4b64f1152
codmoni <doinghmw@gmail.com>
2025-09-25 18:12:55 +0900

    init: setup project using Vite

---
+++ week3/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-purple-500 text-white p-4 font-bold text-center'>
+      <h1>매튜의 고구마 코딩</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
