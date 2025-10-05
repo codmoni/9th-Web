ea6693f1fd770fecee038a10b0dc33b54781e935
moni <muwingky@gmail.com>
2025-10-06 02:31:33 +0900

    Feat: BaseLayout 구현

--- week4/Frontend/src/App.tsx
+++ week4/Frontend/src/App.tsx
@@ -1,19 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import BaseLayout from './Layout/BaseLayout'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
+    element: <BaseLayout />,
+    errorElement: <div>에러 발생</div>,
+    children: [
+      {
+        path: "/",
+        element: <div>홈 화면</div>,
+      },
+      {
+        path: "login",
+        element: <div>로그인 화면</div>,
+      },
+      {
+        path: "signup",
+        element: <div>회원가입 화면</div>,
+      }
+    ]
   },
-  {
-    path: "/login",
-    element: <div>Login</div>,
-  },
-  {
-    path: "/signup",
-    element: <div>SignUp</div>,
-  }
 ])
 
 function App() {
---
+++ week4/Frontend/src/Layout/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Components/Header";
+
+const BaseLayout = () => {
+    return(
+        <>
+        <div className="min-h-screen w-screen bg-black text-white">
+            <Header/>
+            <main className="w-full px-4 pt-6 pb-16">
+                <Outlet />
+            </main>
+        </div>
+        </>
+    )
+}
+
+export default BaseLayout;
