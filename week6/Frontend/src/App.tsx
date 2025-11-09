ff779f6661f79497e919bbab4bf892efc51fbf56
moni <muwingky@gmail.com>
2025-11-10 01:40:18 +0900

    캐싱 잘 되나 테스트

--- week6/Frontend/src/App.tsx
+++ week6/Frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.css'
 import { routes } from './routes/routes'
 
 const router = createBrowserRouter(routes)
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
--- week6/Frontend/src/Pages/music/Musics.tsx
+++ week6/Frontend/src/Pages/music/Musics.tsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { lpKeys } from "../../apis/lps/keys";
 import { getLps, getLpsByUser, getMyLps, getLpsByTag, getLpDetail } from "../../apis/lps/queries";
 
 const Musics = () => {
-    useEffect(() => {
-        const fetchLps = async () => {
-            const lps = await getLpDetail(1, {});
-
-            console.log('Fetched LPs:', lps);
-        };
-
-        fetchLps();
-    }, []);
+    const { data, isPending, isError, error, isFetching, refetch } = useQuery({
+        queryKey: lpKeys.list(),
+        queryFn: getLps
+    });
 
     return (
         <>
             <h1>Musics</h1>
+            {isPending && <p>Loading...</p>}
+            {isError && <p>Error: {error.message}</p>}
+            {data && (
+                <ul>
+                    {data.data.map((music) => (
+                        <li key={music.id}>{music.title}</li>
+                    ))}
+                </ul>
+            )}
         </>
     )
 }
